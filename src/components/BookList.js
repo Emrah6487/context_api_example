d808e7740f95fb6f480197add3668e8676cb483a
Emrah TÜRK <125453114+Emrah6487@users.noreply.github.com>
2023-04-11 12:50:27 +0300

    context default yöntemi bitti

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react'
 import Book from './Book'
 import './BookList.css'
+import BookContext from '../contexts/BookContext'//3.
 
 export class BookList extends Component {
+  //ilk önce oluşturduğumuz bookcontex i static olarak contexType yapalım
+  static contextType = BookContext;//4.
+
   render() {
+
+    const books = this.context//5.
+
     //props tan gelen books arrayi üzerinde map fonk.uygulamak istiyorum ki bunların 
     //tamamını BookList.js içinde oluşturduğum book componetine göndermek
-    const bookList = this.props.books.map((book,i) => {
-        return <Book book={book}
-                     key={i} />
-    })
-   
+
+
     return (
-        <section className="page-section"  id="portfolio">
+      <section className="page-section" id="portfolio">
         <div className="container">
-            <div className="text-center">
-                <h2 className="section-heading text-uppercase">BookFolio</h2>
-                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                
-            </div>
-            <div className="row">
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">BookFolio</h2>
+            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
 
-              {bookList}
+          </div>
+          <div className="row">
 
-            </div>
+            {books.map((book, i) => {
+              return <Book book={book}
+                key={i} />
+            })}
+
+          </div>
         </div>
-    </section>
+      </section>
     )
   }
 }
